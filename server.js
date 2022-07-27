04e36eb7d07f6926cb60c3ebeeaf2f1954c256dc
qeswelan <otaniyici@gmail.com>
2022-07-26 21:45:32 -0400

    add contoller and routes for job and auth

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+// db and authenticateUser
 import connectDB from "./db/connect.js";
+
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
 // middleware
 import notFoundMiddleWare from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleWare);
 app.use(errorHandlerMiddleware);
